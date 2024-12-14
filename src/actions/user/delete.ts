b295af0c416a41ade767a940d3e8a949809d1158
Philip Huynh <wuflerz@outlook.com>
2024-12-14 19:07:35 +0200

    Rewrite

---
+++ src/actions/user/delete.ts
@@ -0,0 +1,22 @@
+"use server"
+import prisma from "@/lib/prisma"
+import { auth } from "@/lib/auth"
+import { headers } from "next/headers"
+
+export default async function deleteUser() {
+    const session = await auth.api.getSession({
+        headers: await headers(),
+    })
+
+    if (!session || !session.user || !session.user.id) {
+        throw new Error('Unauthorized')
+    }
+
+    return await prisma.$transaction(async (tx) => {
+        await tx.user.delete({
+            where: { id: session.user.id }
+        })
+
+        return 'Deleted User'
+    })
+} 
